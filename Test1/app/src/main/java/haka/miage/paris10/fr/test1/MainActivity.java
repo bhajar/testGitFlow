2e37b3467c6715678be7851733c4a7e06594b0fd
bhajar <hajarboulhdir@gmail.com>
2017-01-22 21:55:20 +0100

    added a test of the first android project

---
+++ Test1/app/src/main/java/haka/miage/paris10/fr/test1/MainActivity.java
@@ -0,0 +1,13 @@
+package haka.miage.paris10.fr.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
